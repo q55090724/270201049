b292936c9274173fb87bd4db9e9d4a3f833219e2
q55090724 <umuturem@std.iyte.edu.tr>
2020-10-26 09:57:11 +0000

    example6

---
+++ lab2/example6.py
@@ -0,0 +1,9 @@
+v1 = 80
+v2 = 70
+print("First_Cars_Ve : 80")
+print("Second_Cars_Ve : 70")
+first_distance = 490
+second_distance = 150
+t = (first_distance - second_distance)
+solution = (60 * t) / (v1 + v2)
+print(solution)
