707209e71650bbcb268811fe4cd47b7786717351
q55090724 <umuturem@std.iyte.edu.tr>
2020-11-16 09:46:40 +0000

    i will continue on them

---
+++ lab5/l5example2.py
@@ -0,0 +1,5 @@
+x = int(input("How Many Numbers :"))
+number_even = 0
+
+for i in range(1,x+1):
+  int(input("Enter a Number:"))
---
+++ lab5/l5example3.py
@@ -0,0 +1,11 @@
+x = input("Enter an Integer : ")
+y = input("Enter another Integer : ")
+
+x = list(x)
+y = list(y)
+
+x_digit_num = 0
+y_digit_num = 0
+
+
+
