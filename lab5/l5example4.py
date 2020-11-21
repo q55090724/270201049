253496498cca416977ab0c522bb97ee09f353df7
q55090724 <umuturem@std.iyte.edu.tr>
2020-11-21 19:17:34 +0000

    newchangesss00000

--- lab5/l5example4.py
+++ lab5/l5example4.py
@@ -3,13 +3,12 @@ first_password = "abc123"
 x = input("Please Enter Your Password, (press help to get hint) :")
 
 while x != first_password :
-  print("Wrong")
-  x = input("Enter Your Password Again: ")
-
+    if x == "help" :
+        print(first_password[0] + "*****")
+    else :
+        print("Wrong")
+    x = input("Enter Your Password Again: ")
 if x == first_password :
   print("Welcome")
-
-elif x == "help":
-    print(first_password[0] + "*****")
 
 
