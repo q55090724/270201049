3013feb3f9f6e840efb6768a9c30ffda63df9cb4
q55090724 <umuturem@std.iyte.edu.tr>
2020-11-21 15:37:59 +0000

    exampleeeee24

--- lab5/l5example3.py
+++ lab5/l5example3.py
@@ -1,11 +1,18 @@
-x = input("Enter an Integer : ")
-y = input("Enter another Integer : ")
-
+match = 0
+digit = 0
+x = (input("ENTER THE first NUMBER:"))
+y = (input("ENTER THE second NUMBER:"))
 x = list(x)
 y = list(y)
+x = list(reversed(x))
+y = list(reversed(y))
 
-x_digit_num = 0
-y_digit_num = 0
+while digit< len(x) and digit < len(y):
+  if(x[digit]) == (y[digit]):
+     match += 1
+  digit += 1
+print(match)
+
 
 
 
