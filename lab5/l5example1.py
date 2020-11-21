74a049862fb7d2b441c235b69ed377e7f1a780b8
q55090724 <umuturem@std.iyte.edu.tr>
2020-11-21 14:21:30 +0000

    new examples55

--- lab5/l5example1.py
+++ lab5/l5example1.py
@@ -1,6 +1,6 @@
 a = int(input("Please enter a number to see its table :"))
 
-for i in range (1,11):
+for i in range (1,10):
   print(str(a) + "x" + str(i) + "=", str(a*i))
         
 
--- lab5/l5example2.py
+++ lab5/l5example2.py
@@ -1,5 +1,12 @@
+  
 x = int(input("How Many Numbers :"))
 number_even = 0
-
+number_odd = 0
 for i in range(1,x+1):
-  int(input("Enter a Number:"))
+  a = int(input("Enter a Number:"))
+  if a % 2 == 0:
+    number_even += 1
+  elif a % 2 == 1:
+    number_odd += 1
+
+print((number_even /(number_even + number_odd)) * 100)
