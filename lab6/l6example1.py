1356e51dc2cfb226249fe8dd29c2ccd732065d07
q55090724 <umuturem@std.iyte.edu.tr>
2020-11-25 13:28:44 +0000

    last changes6

--- lab6/l6example1.py
+++ lab6/l6example1.py
@@ -1,22 +1,4 @@
-x = input("Please Enter Your Email Adress :")
-t = "ceng113@example.com"
-z = (t.split("@")[0]).upper()
-y = (t.split("@")[0]).replace('','.')
-previous_ = x.split("@")[0]
-after_ = x.split("@")[1]
-while True:
-  if x == t:
-    print("True")
-    break
-  elif z == previous_:
-    print("True")
-    break
-  elif previous_ ==  y :
-    print("True")
-    break
-  else:
-    print("Wrong")
-    x = input("Please Enter Your Email Adress Again:")
+# i will 
 
   
   
